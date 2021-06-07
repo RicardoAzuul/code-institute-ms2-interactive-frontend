266a4c82cd8e65ee285c3eddd72611e7291df7fe
Richard Blaauw <richard.blaauw@ogd.nl>
2021-06-07 21:48:47 +0200

    Add script.js with comments for functions, change CSS styles for tile padding.

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// function to load game at default setting of normal on page load: we start off loading a board of just 4 tiles, 2 pairs each
+
+// function to load game based on chosen difficulty: easy (3 by 4), medium (4 by 5) or hard (5 by 6)
+
+// function to flip two cards
+
+// function to check the flipped cards to see if they are the same
+
+// function to increase score if flipped cards match
+
+// function to flip cards back if they are not the same
+
+// timer function
