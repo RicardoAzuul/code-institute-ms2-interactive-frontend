e2e04ea3c45d20171ced8800920e294c4e1051b4
Richard Blaauw <richard.blaauw@ogd.nl>
2021-06-17 17:47:11 +0200

    Remove all references to gamedata: I'm not passing difficulty in gamedata, instead I'm adding a class to the button representing the current difficulty. I then use JQuery to get this button using its class, and use that to set the game's difficulty. This commit also adds more code to handle the different difficulties: the mulitplier for random number generation needs to change based on difficulty, though currently only difficulty easy is implemented.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,21 @@ document.addEventListener('DOMContentLoaded', function () {
   // TODO: turn this into code that gets all the buttons
   let startButton = document.getElementById('start-button');
   startButton.addEventListener('click', function () {
+    // we check which button has the btn-lg class: that is our difficulty
+    let largeButton = $('button.btn-lg').text();
+    if (largeButton === 'Easy') {
+      difficulty = 'easy';
+    }
+    else if (largeButton === 'Medium') {
+      difficulty = 'medium';
+    } 
+    else if (largeButton === 'Hard') {
+      difficulty = 'hard';
+    }
+    else {
+      alert('ERROR: Unknown difficulty setting');
+    }
+
     generateSequence(difficulty);
   })
   
@@ -19,33 +34,35 @@ document.addEventListener('DOMContentLoaded', function () {
 // the max layout: 1/2 padding | tile | padding | tile | padding | tile | padding | tile | padding | tile | padding | tile | 1/2 padding --> 6 * padding, 6 * tile
 function createBoard(difficulty) {
   if (difficulty === 'easy') {
+    $('#easy-button').addClass('btn-lg'); // add active class to button so we can use this for the game loop
   }
-
 }
 
 // function to generate a sequence of random numbers, with numbers equating to pictures. This function needs to run when the start button is clicked.
-function generateSequence(difficulty) {  
-  let sequence = [];
-  sequence += Math.floor(Math.random() * 4); // generate random number between 0 and 3 --> we have 4 pictures.
+function generateSequence(difficulty) {
+  let sequence = []; // initialize sequence array
+  let multiplier = 0; // initialize the multiplier we use to generate random numbers
 
-  let gamedata = [difficulty, sequence]; // we're adding the generated sequence to gamedata, which also includes the difficulty setting
-  bopPictures(gamedata);
+  if (difficulty === 'easy') {
+    multiplier = 4;
+  }
+
+  sequence += Math.floor(Math.random() * multiplier); // generate random number between 0 and 3 --> we have 4 pictures.
+  bopPictures(sequence);
 }
 
 // function that uses the generated sequence to bop pictures
-function bopPictures(gamedata) {
-  let sequence = gamedata[1];
+function bopPictures(sequence) {
   $('img')[sequence].animate({
     width: "90%",
     opacity: 0.4
   }, 1000 );
-  checkSequence(gamedata); // we pass the generated sequence to checkSequence after we have bopped the pictures
+  checkSequence(sequence); // we pass the generated sequence to checkSequence after we have bopped the pictures
 }
 
-function checkSequence(gamedata) {
+function checkSequence(sequence) {
   let playerSequence = []; // initialize empty playerSequence array
   let images = $('img'); // get all images: this is an object
-  let sequence = gamedata[1];
 
   for (let image of images) {
     image.addEventListener('click', function() { // add eventlistener to all images
@@ -54,7 +71,7 @@ function checkSequence(gamedata) {
       if (playerSequence === sequence) { // this will only work for sequences of length 1
         alert('Correct!');
         increaseScore();
-        increaseSequenceScore(gamedata);
+        increaseSequenceScore(sequence);
       }
       else {
         alert('Sorry, you got it wrong!');
@@ -71,8 +88,7 @@ function increaseScore() {
 }
 
 // function to increase longest sequence score if the player gets the correct sequence
-function increaseSequenceScore(gamedata) {
-  let sequence = gamedata[1];
+function increaseSequenceScore(sequence) {
   let oldSequenceScore = parseInt($('#longest-sequence').text());
   if (sequence.length > oldSequenceScore) {
     $('#longest-sequence').text(sequence.length);
